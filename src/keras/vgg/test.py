3428ed0e5e6966e6bc692278f1032ed90aa6f3f6
lrojas12 <r.luisaf@gmail.com>
2017-12-09 00:25:08 -0500

    Adding roc curve plotting to testing script

--- src/keras/vgg/test.py
+++ src/keras/vgg/test.py
@@ -5,8 +5,9 @@ from keras.utils.np_utils import to_categorical
 from sklearn.metrics import confusion_matrix
 import itertools
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve,auc
 
-def get_confusion_matrix(y_true, y_pred, class_names):
+def plot_confusion_matrix(y_true, y_pred):
     
     conf_matrix = confusion_matrix(y_true, y_pred)
     
@@ -19,9 +20,9 @@ def get_confusion_matrix(y_true, y_pred, class_names):
     plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
     plt.title(title)
     plt.colorbar()
-    tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=0)
-    plt.yticks(tick_marks, class_names)
+    tick_marks = np.arange(len(class_labels_encoded))
+    plt.xticks(tick_marks, class_labels_encoded, rotation=0)
+    plt.yticks(tick_marks, class_labels_encoded)
 
     threshold = conf_matrix.max() / 2.
     for i, j in itertools.product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
@@ -32,6 +33,39 @@ def get_confusion_matrix(y_true, y_pred, class_names):
     # plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    plt.show()
+
+def plot_roc(y_true, y_pred):
+    
+    # calculate roc and auc
+    false_pos_rate = dict()
+    true_pos_rate = dict()
+    roc_auc = dict()
+    
+    for i in range(num_classes):
+        false_pos_rate[i], true_pos_rate[i], _ = roc_curve(y_true[:,i], y_pred[:,i])
+        roc_auc[i] = auc(false_pos_rate[i], true_pos_rate[i])
+        
+    # plot all
+    
+    cmap = plt.get_cmap('tab10')
+    colors = cmap(np.linspace(0, 1, num_classes))
+    
+    for i, color in zip(range(num_classes), colors):
+        plt.plot(false_pos_rate[i], true_pos_rate[i], lw=2, c=color,
+                    label='c{0} (auc = {1:0.2f})'.format(i, roc_auc[i]))
+
+    # plot random guess roc
+    plt.plot([0, 1], [0, 1], 'k--',color='salmon', lw=2, label='Random Guess')
+    
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title("Receiver Operating Characteristic (ROC) Curve")
+    plt.legend(loc="lower right", fontsize=8)
+    plt.grid()
+    
     plt.show()
 
 # ---------- GET TESTING DATA ----------
@@ -49,7 +83,7 @@ test_generator = datagen.flow_from_directory(
 test_data = np.load('res/vgg_test_features.npy')
 
 test_labels = test_generator.classes # actual class number
-# test_labels_onehot = to_categorical(test_labels, num_classes=num_classes) # class number in onehot
+test_labels_onehot = to_categorical(test_labels, num_classes=num_classes) # class number in onehot
 
 # ---------- TEST MODEL ----------
 
@@ -58,17 +92,14 @@ model.load_weights("res/_top_model_weights.h5")
 
 model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
 
-# loss, acc = model.evaluate(test_data, test_labels_onehot, batch_size=batch_size, verbose=1)
+loss, acc = model.evaluate(test_data, test_labels_onehot, batch_size=batch_size, verbose=1)
 
-# print()
-# print("loss: ", loss)
-# print("accuracy: {:8f}%".format(acc*100))
+print()
+print("loss: ", loss)
+print("accuracy: {:8f}%".format(acc*100))
 
-# predicted = model.predict(test_data)
 predicted = model.predict_classes(test_data)
+predicted_onehot = to_categorical(predicted, num_classes=num_classes)
 
-# convert to array like [ 1.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
-# predicted_onehot = np.zeros_like(predicted)
-# predicted_onehot[np.arange(len(predicted)), predicted.argmax(1)] = 1
-
-get_confusion_matrix(test_labels, predicted, class_labels_encoded)
+plot_confusion_matrix(test_labels, predicted)
+plot_roc(test_labels_onehot, predicted_onehot)
